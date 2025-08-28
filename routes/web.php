dea55db1a06cba85595104d6100747bbfd8232b9
nabaraj999 <nabarajacharya999@gmail.com>
2025-08-28 20:51:01 +0545

    feat: Reintroduce ticket model and migration with updated structure

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\PageController;
-use App\Http\Controllers\TicketController;
+ use App\Http\Controllers\TicketController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
-  Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+
+
+Route::post('/', [TicketController::class, 'store'])->name('tickets.store');
